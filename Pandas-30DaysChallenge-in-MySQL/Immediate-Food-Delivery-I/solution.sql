30745c33928e5c3447c763222b5688cf79b2dc83
Sudipta Mondal <69045975+mondalsudipta@users.noreply.github.com>
2023-08-10 21:34:07 +0600

    solution added

---
+++ Pandas-30DaysChallenge-in-MySQL/Immediate-Food-Delivery-I/solution.sql
@@ -0,0 +1,17 @@
+SELECT 
+  ROUND(
+    (
+      SELECT 
+        COUNT(delivery_id) 
+      FROM 
+        Delivery 
+      WHERE 
+        order_date = customer_pref_delivery_date
+    ) / (
+      SELECT 
+        COUNT(delivery_id) 
+      FROM 
+        Delivery
+    )* 100, 
+    2
+  ) AS immediate_percentage;
